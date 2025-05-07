a1596549d0594e0d3f43068755f8f71a405f8c4d
Mihail Yonchev <mihail@latticeflow.ai>
2025-05-07 18:27:32 +0200

    feat: deploy

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    unoptimized: true,
+  },
+  assetPrefix: isProd ? "/trakable/" : "",
+  basePath: isProd ? "/trakable" : "",
+  output: "export",
 };
 
 export default nextConfig;
